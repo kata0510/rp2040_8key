deef942a05f1b0b946b3f903065424dae0610bad
kata0510 <yuchi.kbd@gmail.com>
2023-12-30 12:31:13 +0900

    keymap_update

--- firmware/rp2040_8key/keymaps/default/keymap.c
+++ firmware/rp2040_8key/keymaps/default/keymap.c
@@ -2,7 +2,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ortho_2x8(
-        KC_P1,  KC_P2,  KC_P3,  KC_P4, KC_P5,  KC_P6,  KC_P7,  KC_P8,
+        KC_1,  KC_2,  KC_3,  KC_4, KC_5,  KC_6,  KC_7,  KC_8,
         KC_A,  KC_B,  KC_C,  KC_D, KC_E,  KC_F,  KC_G,  KC_H
     )
 };
